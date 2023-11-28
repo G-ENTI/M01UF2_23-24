5f4b5155778289f1589c60106c2b85850e205783
Guillermo Gómez <guillermo.gomez@enti.cat>
2023-11-28 09:59:13 +0100

    eliminados archivos temporales de VIM

--- eftp/client.sh
+++ eftp/client.sh
@@ -1,31 +1,41 @@
 echo "CLIENTE DE EFTP"
-IP=`ip address | grep inet | tail -n 2 | head -n 1 | cut -d " " -f 6 | cut -d "/" -f 1` 
+# Conseguimos la ip de la maquina, el puerto y seteamos el timeout
+IP=`ip address | grep inet | tail -n 2 | head -n 1 | cut -d " " -f 6 | cut -d "/" -f 1`
+PORT=3333 
 TIMEOUT=1
+# Una vez el servidor puesto en escucha empezamos enviando el header
 echo "(1). SEND"
 
-echo "EFTP 1.0" | nc $IP 3333
+echo "EFTP 1.0" | nc $IP $PORT
+# Una vez enviado el header entramos en escucha
 echo "(2.) LISTEN"
-DATA=` nc -l -p 3333 -w $TIMEOUT `
+DATA=` nc -l -p $PORT -w $TIMEOUT `
 sleep 1
+# Comprobamos que el servidor ha recibido correctamente el header, en caso de no hacerlo nor dara error 1
 if [ "$DATA" != "OK_HEADER" ]
 then
 	echo "ERROR 1: BAD_HEADER"
 	exit 1
 fi
+# En caso de ser correcto, continuara el proceso enviando el handshake
 echo "(5.) SEND"
-echo "BOOOM" | nc $IP 3333
+echo "BOOOM" | nc $IP $PORT
+# Una vez enviado el handshake nos ponemos en escucha
 echo "(6.)LISTEN"
-DATA=` nc -l -p 3333 -w $TIMEOUT `
+# Almacenamos lo escuchado dentro de la variable DATA
+DATA=` nc -l -p $PORT -w $TIMEOUT `
 echo "(9).TEST"
+# Testeo si el servidor ha recibido correctamente el hanshake, en caso de no hacerlo nos dara el error 2
 sleep 1
 if [ "$DATA" != "OK_HANDSHAKE" ]
 then
 	echo "ERROR 2: BAD_HANDSHAKE"
 	exit 2
 fi
+# En caso de ser correcto seguira el proceso enviando el nombre del archivo junto a el MD5 de su nombre
 FILE_NAME="fary.txt"
 FILE_MD5=`echo "$FILE_NAME" | md5sum  | cut -d " " -f 1`
 echo "(10). SEND"
-echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP 3333
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP $PORT
 echo "(11). LISTEN"
-DATA=`nc -l -p 3333 -w $TIMEOUT`
+DATA=`nc -l -p $PORT -w $TIMEOUT`
--- eftp/server.sh
+++ eftp/server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "SERVIDOR DE EFTP"
 IP=`ip address | grep inet | tail -n 2 | head -n 1 | cut -d " " -f 6 | cut -d "/" -f 1`
+PORT=3333
 TIMEOUT=1
 echo "(0). Listen"
 
 # Almacenamos el mensaje de netcat dentro de la variable "DATA"
-DATA=` nc -l -p 3333 -w $TIMEOUT `
+DATA=` nc -l -p $PORT -w $TIMEOUT `
 # Mostramos por la salida estadar lo almacenado en la variable "DATA"
 echo "$DATA"
 sleep 1
@@ -17,15 +18,15 @@ echo "(3). TEST & SEND"
 if [ "$DATA" != "EFTP 1.0" ]
 then
 	echo "ERROR!!!"
-	echo "KO__HEADER" | nc $IP 3333
+	echo "KO__HEADER" | nc $IP $PORT
 	exit 1
 fi
 # En caso de ser correcto, continua enviando el mensaje "OK_HEADER" al cliente
-echo "OK_HEADER" | nc $IP 3333
+echo "OK_HEADER" | nc $IP $PORT
 # Ponemos el servidor en escucha
 echo "(4.) LISTEN"
 # Almacenamos la escucha del servidor dentro de la variable "DATA"
-DATA=` nc -l -p 3333 -w $TIMEOUT `
+DATA=` nc -l -p $PORT -w $TIMEOUT `
 sleep 1
 # Comprobamos que el HANDSHAKE es correcto
 echo "(7.) TEST & SEND"
@@ -33,20 +34,26 @@ echo "(7.) TEST & SEND"
 if [ "$DATA" != "BOOOM" ]
 then
 	echo "ERROR 2: BAD_HANDSHAKE"
-	echo "KO_HANDSHAKE" | nc $IP 3333
+	echo "KO_HANDSHAKE" | nc $IP $PORT
 	exit 2
 fi
-echo "OK_HANDSHAKE" | nc $IP 3333
+# En caso de ser correcto enviara el mensaje "OK_HANDSHAKE al cliente"
+echo "OK_HANDSHAKE" | nc $IP $PORT
+# Inmediantamente despues entramos en escucha
 echo "(8).Listen"
-DATA=` nc -l -p 3333 -w $TIMEOUT `
+# Almacenamos la entrada de datos dentro de la variable "DATA"
+DATA=` nc -l -p $PORT -w $TIMEOUT `
 echo "(12).TEST & STORE & SEND"
+# Cortamos el mensaje entrante por data y nos guardamos el resultado del corte dentro de la variable "PREFIX"
 PREFIX=`echo $DATA | cut -d " " -f 1`
+# Si el prefix es incorrecto, enviara el mensaje "KO_FILENAME" y terminara el proceso en codigo 3
 if [ "$PREFIX" != "FILE_NAME" ]
 then
 	echo "ERROR 3: BAD FILENAME PREFIX"
 	sleep 1
-	echo "KO_FILE_NAME" | nc $IP 3333
+	echo "KO_FILE_NAME" | nc $IP $PORT
 	exit 3
 fi
-echo "OK_FILE_NAME" | nc $IP 3333
+# En caso de ser correcto, enviara el mensaje "OK_FILE_NAME"
+echo "OK_FILE_NAME" | nc $IP $PORT
 
