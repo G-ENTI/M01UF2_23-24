9d437b319a31772d0043ab5ed3f6dfd4087531b4
Guillermo Gómez <guillermo.gomez@enti.cat>
2023-11-28 09:58:08 +0100

    cambiados el hardcodeo de puertos, se han añadido nuevos comentarios en el codigo y añadido el hash MD5 de el nombre del archivo almacenado en una variable

--- eftp/client.sh
+++ eftp/client.sh
@@ -1,10 +1,11 @@
 echo "CLIENTE DE EFTP"
-
+IP=`ip address | grep inet | tail -n 2 | head -n 1 | cut -d " " -f 6 | cut -d "/" -f 1` 
+TIMEOUT=1
 echo "(1). SEND"
 
-echo "EFTP 1.0" | nc localhost 3333
+echo "EFTP 1.0" | nc $IP 3333
 echo "(2.) LISTEN"
-DATA=` nc -l -p 3333 -w 0 `
+DATA=` nc -l -p 3333 -w $TIMEOUT `
 sleep 1
 if [ "$DATA" != "OK_HEADER" ]
 then
@@ -12,9 +13,9 @@ then
 	exit 1
 fi
 echo "(5.) SEND"
-echo "BOOOM" | nc localhost 3333
+echo "BOOOM" | nc $IP 3333
 echo "(6.)LISTEN"
-DATA=` nc -l -p 3333 -w 0 `
+DATA=` nc -l -p 3333 -w $TIMEOUT `
 echo "(9).TEST"
 sleep 1
 if [ "$DATA" != "OK_HANDSHAKE" ]
@@ -22,7 +23,9 @@ then
 	echo "ERROR 2: BAD_HANDSHAKE"
 	exit 2
 fi
+FILE_NAME="fary.txt"
+FILE_MD5=`echo "$FILE_NAME" | md5sum  | cut -d " " -f 1`
 echo "(10). SEND"
-echo "FILE_NAME fary.txt" | nc localhost 3333
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP 3333
 echo "(11). LISTEN"
-DATA=`nc -l -p 3333 -w 0`
+DATA=`nc -l -p 3333 -w $TIMEOUT`
--- eftp/server.sh
+++ eftp/server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "SERVIDOR DE EFTP"
-
+IP=`ip address | grep inet | tail -n 2 | head -n 1 | cut -d " " -f 6 | cut -d "/" -f 1`
+TIMEOUT=1
 echo "(0). Listen"
 
 # Almacenamos el mensaje de netcat dentro de la variable "DATA"
-DATA=` nc -l -p 3333 -w 0 `
+DATA=` nc -l -p 3333 -w $TIMEOUT `
 # Mostramos por la salida estadar lo almacenado en la variable "DATA"
 echo "$DATA"
 sleep 1
@@ -16,15 +17,15 @@ echo "(3). TEST & SEND"
 if [ "$DATA" != "EFTP 1.0" ]
 then
 	echo "ERROR!!!"
-	echo "KO__HEADER" | nc localhost 3333
+	echo "KO__HEADER" | nc $IP 3333
 	exit 1
 fi
 # En caso de ser correcto, continua enviando el mensaje "OK_HEADER" al cliente
-echo "OK_HEADER" | nc localhost 3333
+echo "OK_HEADER" | nc $IP 3333
 # Ponemos el servidor en escucha
-echo "(4. LISTEN)"
+echo "(4.) LISTEN"
 # Almacenamos la escucha del servidor dentro de la variable "DATA"
-DATA=` nc -l -p 3333 -w 0 `
+DATA=` nc -l -p 3333 -w $TIMEOUT `
 sleep 1
 # Comprobamos que el HANDSHAKE es correcto
 echo "(7.) TEST & SEND"
@@ -32,24 +33,20 @@ echo "(7.) TEST & SEND"
 if [ "$DATA" != "BOOOM" ]
 then
 	echo "ERROR 2: BAD_HANDSHAKE"
-	echo "KO_HANDSHAKE" | nc localhost 3333
+	echo "KO_HANDSHAKE" | nc $IP 3333
 	exit 2
 fi
-echo "OK_HANDSHAKE" | nc localhost 3333
+echo "OK_HANDSHAKE" | nc $IP 3333
 echo "(8).Listen"
-DATA=` nc -l -p 3333 -w 0 `
+DATA=` nc -l -p 3333 -w $TIMEOUT `
 echo "(12).TEST & STORE & SEND"
 PREFIX=`echo $DATA | cut -d " " -f 1`
 if [ "$PREFIX" != "FILE_NAME" ]
 then
 	echo "ERROR 3: BAD FILENAME PREFIX"
 	sleep 1
-	echo "KO_FILE_NAME" | nc localhost 3333
+	echo "KO_FILE_NAME" | nc $IP 3333
 	exit 3
 fi
-
-FILE_NAME=` echo $DATA | cut -d " " -f 2 `
-
-
-
+echo "OK_FILE_NAME" | nc $IP 3333
 
